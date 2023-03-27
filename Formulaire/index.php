8350045a36bd9e33a332b2f8435493a3d49c7228
Firelag <jordan.vincent085@gmail.com>
2023-03-27 14:52:06 +0200

    commit 2

--- Formulaire/index.php
+++ Formulaire/index.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    
+
+
+
 <?php
 require 'form.php';
 
@@ -24,3 +38,7 @@ $form = new Form($_POST);
 ?>
 
 </form>
+
+
+</body>
+</html>
